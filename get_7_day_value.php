a038b39d26a5f0fc24c053205c57bd79ff0080e2
buckgnarly <buckgnarly@gmail.com>
2017-02-17 08:13:47 -0600

    Updated the 7 day chart to start with minimum values instead of zero if null.

--- get_7_day_value.php
+++ get_7_day_value.php
@@ -16,6 +16,14 @@ printf ("  \"rows\": [\n");
 $timeend = time();
 $timestart = $timeend - (60 * 60 * 24 * 7);
 $first = true;
+/* Get our minimums for the time frame */
+$res = $sql->query ("SELECT MIN(value) as min FROM (".
+    "SELECT AVG(value) as value FROM balance WHERE (".
+    "timestamp>".$timestart." AND timestamp<".$timeend.") GROUP BY timestamp) t");
+$minval = $res->fetch_array()[0];
+$res = $sql->query ("SELECT MIN(last) as min FROM market_data WHERE (".
+"timestamp>".$timestart." AND timestamp<".$timeend." AND exchange='BPI')");
+$minbpi = $res->fetch_array()[0];
 for ($time=$timestart; $time<$timeend; $time+=(120 * 60)) {
     $tend = $time + (60 * 120);
     $res = $sql->query ("SELECT COUNT(value) as count, AVG(value) as value FROM balance ".
@@ -25,7 +33,7 @@ for ($time=$timestart; $time<$timeend; $time+=(120 * 60)) {
     $dates = sprintf ("Date(%d, %d, %d, %d, %d)", $dater["year"], $dater["mon"] - 1,
     $dater["mday"], $dater["hours"], $dater["minutes"]);
     if ($res->num_rows == 0) {
-        if ($first) $total = 0;
+        if ($first) $total = $minval;
         else $total = "null";
     } else {
         $total = 0;
@@ -37,7 +45,7 @@ for ($time=$timestart; $time<$timeend; $time+=(120 * 60)) {
     $res = $sql->query ("SELECT COUNT(last) as count, AVG(last) as value FROM market_data ".
     "WHERE (exchange='BPI' AND timestamp>".$time." AND timestamp<=".$tend.") GROUP BY exchange");    
     if ($res->num_rows == 0) {
-        if ($first) $bpi = 0;
+        if ($first) $bpi = $minbpi;
         else $bpi = "null";
     } else {
         $bpi = sprintf ("%4.2f", $res->fetch_assoc()["value"]);
