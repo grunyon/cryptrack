c099ec977c0cc982bfca3f6b8fabaa1cd06bcd83
buckgnarly <buckgnarly@gmail.com>
2017-02-21 13:35:43 -0600

    Update SQL query for more accuracy.

--- get_7_day_value.php
+++ get_7_day_value.php
@@ -26,21 +26,20 @@ $res = $sql->query ("SELECT MIN(last) as min FROM market_data WHERE (".
 $minbpi = $res->fetch_array()[0];
 for ($time=$timestart; $time<$timeend; $time+=(120 * 60)) {
     $tend = $time + (60 * 120);
-    $res = $sql->query ("SELECT COUNT(value) as count, AVG(value) as value FROM balance ".
-    "WHERE (timestamp>".$time." AND timestamp<=".$tend.") GROUP BY name");    
+    $qry = "SELECT AVG(value) as value FROM (SELECT SUM(value) as value ".
+        "FROM balance WHERE (timestamp>".$time." AND timestamp<=".$tend.") ".
+        "GROUP BY timestamp) t;";
+    $res = $sql->query($qry);
     $timestr = strftime("%R", $time);
     $dater = getdate ($time);
     $dates = sprintf ("Date(%d, %d, %d, %d, %d)", $dater["year"], $dater["mon"] - 1,
     $dater["mday"], $dater["hours"], $dater["minutes"]);
-    if ($res->num_rows == 0) {
+    $data= $res->fetch_assoc();
+    if ($data["value"] == "") {
         if ($first) $total = $minval;
         else $total = "null";
     } else {
-        $total = 0;
-        while ($data = $res->fetch_assoc()) {
-            $total += $data["value"];
-        }
-        $total = sprintf ("%4.2f", $total);
+        $total = sprintf ("%4.2f", $data["value"]);
     }
     $res = $sql->query ("SELECT COUNT(last) as count, AVG(last) as value FROM market_data ".
     "WHERE (exchange='BPI' AND timestamp>".$time." AND timestamp<=".$tend.") GROUP BY exchange");    
