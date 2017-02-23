8f4fbee4e215242c7d3ee18f57ffd9d890a502c8
buckgnarly <buckgnarly@gmail.com>
2017-02-23 07:00:01 -0600

    Added an average daily change value.

--- equity_growth.php
+++ equity_growth.php
@@ -26,13 +26,44 @@ $res = $sql->query ("SELECT SUM(value) AS value FROM balance WHERE ".
 "(timestamp>=".$month.") GROUP BY timestamp ORDER BY timestamp ASC LIMIT 1");
 $month_equity = $res->fetch_assoc()["value"];
 /* Get our earliest equity value */
-$res = $sql->query ("SELECT SUM(value) AS value FROM balance ".
+$res = $sql->query ("SELECT timestamp,SUM(value) AS value FROM balance ".
 " GROUP BY timestamp ORDER BY timestamp ASC LIMIT 1");
-$earliest_equity = $res->fetch_assoc()["value"];
+$data = $res->fetch_assoc();
+$earliest_equity = $data["value"];
+$earliest_time = $data["timestamp"];
+/* Get our average daily change */
+$sum = 0.0;
+for ($i = $now, $c = 0; $i>$earliest_time; $i -= (60 * 60 * 24), $c++) {
+    $etime = $i;
+    $stime = $etime - (60 * 60 * 24) + 1;
+    $qry = "SELECT MIN(timestamp) as start,MAX(timestamp) as end ".
+        "FROM balance WHERE (timestamp>".$stime." AND timestamp<".$etime.")";    
+    $res = $sql->query ($qry);
+    $timed = $res->fetch_assoc();
+    $startt = $timed["start"];
+    $endt = $timed["end"];
+    $qry = "SELECT SUM(value) AS value FROM balance WHERE (timestamp=".$startt.")";
+    $res = $sql->query ($qry);
+    $startv = $res->fetch_assoc()["value"];
+    $qry = "SELECT SUM(value) AS value FROM balance WHERE (timestamp=".$endt.")";
+    $res = $sql->query ($qry);
+    $endv = $res->fetch_assoc()["value"];
+    $sum+=($endv - $startv);
+}
+$avg_daily = $sum/$c;
 ?>
 <table width="100%">
 <tr>
 <td><b>Equity Change: </b></td>
+<td>Average Daily Change: 
+<?php
+if ($avg_daily > 0) {
+    printf ("<span class=\"bpositive\">+$%4.2f</span>", $avg_daily);
+} else {
+    printf ("<span class=\"bnegative\">-$%4.2f</span>", abs($avg_daily));
+}
+?>
+</td>
 <td>24 Hour Change: 
 <?php
 $change = $current_equity - $day_equity;
